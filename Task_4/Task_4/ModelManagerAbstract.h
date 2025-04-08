f536019b59a7dba452d0ad18f42ec272bde23dd3
AlexSoulJk <alex.solomatow2016@yandex.ru>
2025-04-08 18:58:47 +0300

    Add Lab4

---
+++ Task_4/Task_4/ModelManagerAbstract.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <DirectXMath.h>
+#include <d3d11.h>
+using namespace DirectX;
+class ModelManagerAbstract
+{
+public:
+
+    struct Vertex {
+        DirectX::XMFLOAT3 pos;
+        DirectX::XMFLOAT2 texCoord;
+    };
+    DirectX::XMMATRIX GetModelMatrix() const { return m_modelMatrix; }
+    ModelManagerAbstract(ID3D11DeviceContext* context);
+    virtual ~ModelManagerAbstract() {
+        if (m_pVertexBuffer) m_pVertexBuffer->Release();
+        if (m_pIndexBuffer) m_pIndexBuffer->Release();
+        if (m_pModelBuffer) m_pModelBuffer->Release();
+    }
+    virtual HRESULT InitModel(ID3D11Device* device) = 0;
+    virtual void Update(float dt) = 0;
+    virtual void Render() = 0;
+protected:
+
+    ID3D11Buffer* m_pModelBuffer = nullptr;
+    ID3D11Buffer* m_pVertexBuffer = nullptr;
+    ID3D11Buffer* m_pIndexBuffer = nullptr;
+    ID3D11DeviceContext* m_context = nullptr;
+
+    DirectX::XMMATRIX m_modelMatrix = DirectX::XMMatrixIdentity();
+    float m_rotationAngle = 0.0f;
+};
+
